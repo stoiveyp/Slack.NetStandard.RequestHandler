0e2e56aa4d58ec6db96e8f219177064a18dc1c9a
Steven Pears <steven.pears@live.co.uk>
2021-05-22 12:32:46 +0100

    Initial Modal stack (#1)
    * Add modal and modal stack
    
    * Add initial modal tests
    
    * Add parent model
    version bump

---
+++ Slack.NetStandard.RequestHandler/Handlers/Modal.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Slack.NetStandard.Interaction;
+using Slack.NetStandard.Objects;
+
+namespace Slack.NetStandard.RequestHandler.Handlers
+{
+    public abstract class Modal: IParentModal
+    {
+        protected Modal(string callbackId):this(callbackId, bp => false){}
+
+        protected Modal(string callbackId, params string[] updateActionIds):this(callbackId, bp => IsBlockAction(bp, updateActionIds))
+        {
+            
+        }
+
+        protected Modal(string callbackId, Func<SlackContext, bool> updateCheck)
+        {
+            CallbackId = callbackId;
+            UpdateCheck = updateCheck;
+        }
+
+        public Func<SlackContext, bool> UpdateCheck { get; set; }
+
+        public readonly string ModalHandlerId = Guid.NewGuid().ToString("N");
+        public Dictionary<string,Modal> Modals = new ();
+
+        public string CallbackId { get; set; }
+        public virtual bool CanHandle(SlackContext context)
+        {
+            if (IsViewSubmission(context))
+            {
+                context.Items[ModalHandlerId] = "submit";
+                return true;
+
+            }
+
+            if (UpdateCheck(context))
+            {
+                context.Items[ModalHandlerId] = "update";
+                return true;
+            }
+
+            return Modals.Any(m => m.Value.CanHandle(context));
+        }
+
+        protected virtual bool IsViewSubmission(SlackContext context)
+        {
+            return context.Interaction is ViewSubmissionPayload view && view.View.CallbackId == CallbackId;
+        }
+
+        protected static bool IsBlockAction(SlackContext context, string[] actionIds)
+        {
+            return actionIds.Any() && context.Interaction is BlockActionsPayload blocks &&
+                   blocks.Actions.Any(pa => actionIds.Contains(pa.ActionId));
+        }
+
+        public virtual Task<ResponseAction> Submit(ViewSubmissionPayload payload, SlackContext context, IParentModal parent = null)
+        {
+            return Task.FromResult(default(ResponseAction));
+        }
+
+        public virtual Task<ResponseAction> Update(BlockActionsPayload blockActions, SlackContext context, IParentModal parent = null)
+        {
+            return Task.FromResult(default(ResponseAction));
+        }
+
+        public abstract View GenerateView(object context = null);
+
+        protected virtual Task<ResponseAction> HandleFromParent(SlackContext context, IParentModal parent)
+        {
+            if (!context.Items.ContainsKey(ModalHandlerId))
+            {
+                return Modals.First(m => context.Items.ContainsKey(m.Value.ModalHandlerId)).Value.HandleFromParent(context, this);
+            }
+
+            return (string)context.Items[ModalHandlerId] == "submit" ?
+                Submit((ViewSubmissionPayload)context.Interaction, context, parent) :
+                Update((BlockActionsPayload)context.Interaction, context, parent);
+        }
+
+        public virtual Task<ResponseAction> Handle(SlackContext context)
+        {
+            if (!context.Items.ContainsKey(ModalHandlerId))
+            {
+                return Modals.First(m => context.Items.ContainsKey(m.Value.ModalHandlerId)).Value.HandleFromParent(context, this);
+            }
+
+            return (string)context.Items[ModalHandlerId] == "submit" ? 
+                Submit((ViewSubmissionPayload)context.Interaction, context) : 
+                Update((BlockActionsPayload) context.Interaction, context);
+        }
+    }
+}
---
+++ Slack.NetStandard.RequestHandler/Handlers/ModalStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Slack.NetStandard.Interaction;
+
+namespace Slack.NetStandard.RequestHandler.Handlers
+{
+    public abstract class ModalStack<TResponse>:ISlackRequestHandler<TResponse>
+    {
+        protected ModalStack(Modal initialView)
+        {
+            InitialView = initialView;
+        }
+
+        public Modal InitialView { get; set; }
+
+        public abstract Task<TResponse> ConvertResponseAction(ResponseAction responseAction);
+
+        public abstract Task<TResponse> NoResponse();
+
+        public bool CanHandle(SlackContext context)
+        {
+            return InitialView.CanHandle(context);
+        }
+
+        public async Task<TResponse> Handle(SlackContext context)
+        {
+            var task = InitialView.Handle(context);
+            if (task == null)
+            {
+                return await NoResponse();
+            }
+
+            var result = await task;
+            return await (result != null ? ConvertResponseAction(result) : NoResponse());
+        }
+    }
+}
